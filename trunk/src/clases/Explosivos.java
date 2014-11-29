f3960be1bcefc9205276ee9b3b99252bc87b6bc4
genzo16@gmail.com <genzo16@gmail.com@22a2f680-ad36-1210-6afb-c2ed63f4e533>
2014-11-29 23:30:14 +0000

    Logica de "PoliticasDeEmpresa" aplicadas al pedido. La comprobación de productos validados queda comentada pero terminada, descomentar para usar(tiene un TODO que la señala).

--- trunk/src/clases/Explosivos.java
+++ trunk/src/clases/Explosivos.java
@@ -1,14 +1,14 @@
 package clases;
 
-public class Explosivos implements PoliticasDeEnvio{
-
+public class Explosivos implements PoliticasDeEnvio
+{
 	public Explosivos (){}
 
 	@Override
-	public String Evaluar(Mercaderia _mercaderia) {
+	public String Evaluar(Pedido _pedido) 
+	{
+		if(_pedido.getManifiesto().contains("Explosivo"))
+			return "Explosivo-No se puede transportar";
 		return null;
-		// TODO Auto-generated method stub
-		
-	}
-	
+	}	
 }
